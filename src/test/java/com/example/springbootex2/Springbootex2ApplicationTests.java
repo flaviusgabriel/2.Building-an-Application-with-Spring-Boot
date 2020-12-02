4465090e14059fa1da2868ea668bf34abd4dc2dd
Flavius <flaviusstasac@yahoo.com>
2020-12-02 16:55:10 +0200

    info

---
+++ src/test/java/com/example/springbootex2/Springbootex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootex2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
